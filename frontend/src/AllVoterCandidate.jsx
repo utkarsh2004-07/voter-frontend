57c66261e63f35d832538aaa64167865cbf4bfcc
utkarsh singh <121781121+utkarsh2004-07@users.noreply.github.com>
2024-11-09 17:49:22 +0530

    Update AllVoterCandidate.jsx

--- frontend/src/AllVoterCandidate.jsx
+++ frontend/src/AllVoterCandidate.jsx
@@ -4,7 +4,7 @@ import { jsPDF } from 'jspdf';
 
 const UserSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [bootNoFilter] = useState('297 ISAAC Newton Globel School, Room.1, Ramedy Vasai ( W )');
+  const [bootNoFilter] = useState('298 ISAAC Newton Globel School Ramedy Vasai ( W ) Room No. 2,Room No.2');
   const [userData, setUserData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
