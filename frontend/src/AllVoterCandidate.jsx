af031388722f05767a732eeffd0c64f0ca744370
utkarsh singh <121781121+utkarsh2004-07@users.noreply.github.com>
2024-11-09 15:34:48 +0530

    Update AllVoterCandidate.jsx

--- frontend/src/AllVoterCandidate.jsx
+++ frontend/src/AllVoterCandidate.jsx
@@ -4,7 +4,7 @@ import { jsPDF } from 'jspdf';
 
 const UserSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [bootNoFilter] = useState('242 Agri Samaj Shikshan Sanstha BJ Kini High School, Room No. 1');
+  const [bootNoFilter] = useState('243 Agri Samaj Shikshan Sanstha B J Kini High School, Room.2');
   const [userData, setUserData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
