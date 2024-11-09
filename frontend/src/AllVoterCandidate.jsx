ae96dbee218d868519676118df41e9b26adae9fe
utkarsh singh <121781121+utkarsh2004-07@users.noreply.github.com>
2024-11-09 17:33:59 +0530

    Update AllVoterCandidate.jsx

--- frontend/src/AllVoterCandidate.jsx
+++ frontend/src/AllVoterCandidate.jsx
@@ -4,7 +4,7 @@ import { jsPDF } from 'jspdf';
 
 const UserSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [bootNoFilter] = useState('286 St. Anthony Convent, Koliwada, Room No. 1, Hall 1 ( Gr. Floor ),Malonde vasai');
+  const [bootNoFilter] = useState('287 St. Anthony Convent, Koliwada, Room No. 1, Room No. 2,Malonde vasai');
   const [userData, setUserData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
